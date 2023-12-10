77615d85c7e622a5a7bf90013521798f54159e37
fajar sidik priatna <jardik.oryza@gmail.com>
2023-12-10 11:52:02 +0700

    refactor livewire 2

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -24,37 +24,37 @@ ipcRenderer.on('native-event', (event, data) => {
     window.Livewire.dispatch('nativephp:' + data.event, data.payload);
   }
 
+  // add support for livewire 2
   // @ts-ignore
-  if (!window.livewire) {
-    return;
+  if (window.livewire) {
+    // @ts-ignore
+    window.livewire.components.components().forEach(component => {
+      if (Array.isArray(component.listeners)) {
+        component.listeners.forEach(event => {
+          if (event.startsWith('native')) {
+            let event_parts = event.split(/(native:|native-)|:|,/)
+
+            if (event_parts[1] == 'native:') {
+              event_parts.splice(2, 0, 'private', undefined, 'nativephp', undefined)
+            }
+
+            let [
+              s1,
+              signature,
+              channel_type,
+              s2,
+              channel,
+              s3,
+              event_name,
+            ] = event_parts
+
+            if (data.event === event_name) {
+              // @ts-ignore
+              window.livewire.emit(event, data.payload)
+            }
+          }
+        })
+      }
+    })
   }
-  // @ts-ignore
-  window.livewire.components.components().forEach(component => {
-    if (Array.isArray(component.listeners)) {
-      component.listeners.forEach(event => {
-        if (event.startsWith('native')) {
-          let event_parts = event.split(/(native:|native-)|:|,/)
-
-          if (event_parts[1] == 'native:') {
-            event_parts.splice(2, 0, 'private', undefined, 'nativephp', undefined)
-          }
-
-          let [
-            s1,
-            signature,
-            channel_type,
-            s2,
-            channel,
-            s3,
-            event_name,
-          ] = event_parts
-
-          if (data.event === event_name) {
-            // @ts-ignore
-            window.livewire.emit(event, data.payload)
-          }
-        }
-      })
-    }
-  })
 })
