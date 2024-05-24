49e0f04a9233e7092aa92d948b7d59fc3f99c613
Alvin Lau <sarukomine@gmail.com>
2024-05-24 08:36:43 +0800

    Make traffic light position editable for browser window (#28)
    * Make traffic light position editable for browser window
    
    * Build js file

--- dist/server/api/window.js
+++ dist/server/api/window.js
@@ -29,10 +29,10 @@ router.post('/position', (req, res) => {
     res.sendStatus(200);
 });
 router.post('/reload', (req, res) => {
-  var _a;
-  const { id } = req.body;
-  (_a = state_1.default.windows[id]) === null || _a === void 0 ? void 0 : _a.reload();
-  res.sendStatus(200);
+    var _a;
+    const { id } = req.body;
+    (_a = state_1.default.windows[id]) === null || _a === void 0 ? void 0 : _a.reload();
+    res.sendStatus(200);
 });
 router.post('/close', (req, res) => {
     const { id } = req.body;
@@ -69,7 +69,7 @@ router.post('/always-on-top', (req, res) => {
     res.sendStatus(200);
 });
 router.post('/open', (req, res) => {
-    let { id, x, y, frame, width, height, minWidth, minHeight, maxWidth, maxHeight, focusable, hasShadow, url, resizable, movable, minimizable, maximizable, closable, title, alwaysOnTop, titleBarStyle, vibrancy, backgroundColor, transparency, showDevTools, fullscreen, kiosk, autoHideMenuBar, } = req.body;
+    let { id, x, y, frame, width, height, minWidth, minHeight, maxWidth, maxHeight, focusable, hasShadow, url, resizable, movable, minimizable, maximizable, closable, title, alwaysOnTop, titleBarStyle, trafficLightPosition, vibrancy, backgroundColor, transparency, showDevTools, fullscreen, kiosk, autoHideMenuBar, } = req.body;
     if (state_1.default.windows[id]) {
         state_1.default.windows[id].show();
         state_1.default.windows[id].focus();
@@ -93,6 +93,7 @@ router.post('/open', (req, res) => {
         closable,
         hasShadow,
         titleBarStyle,
+        trafficLightPosition,
         vibrancy,
         focusable,
         autoHideMenuBar }, (process.platform === 'linux' ? { icon: state_1.default.icon } : {})), { webPreferences: {
