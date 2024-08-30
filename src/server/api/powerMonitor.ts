49382d282ca0fbcc352262aa0a2e55d83cd92588
Dan Johnson <dan@try.be>
2024-08-30 21:34:06 +0100

    feat: add event listeners

--- src/server/api/powerMonitor.ts
+++ src/server/api/powerMonitor.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { powerMonitor } from 'electron'
+import { notifyLaravel } from '../utils';
 const router = express.Router();
 
 router.get('/get-system-idle-state', (req, res) => {
@@ -25,5 +26,41 @@ router.get('/is-on-battery-power', (req, res) => {
         result: powerMonitor.isOnBatteryPower(),
     })
 });
+
+powerMonitor.addListener('on-ac', () => {
+    notifyLaravel("events", {
+        event: `\\Native\\Laravel\\Events\\PowerMonitor\\PowerStateChanged`,
+        payload: {
+            state: 'on-ac'
+        }
+    });
+})
+
+powerMonitor.addListener('on-battery', () => {
+    notifyLaravel("events", {
+        event: `\\Native\\Laravel\\Events\\PowerMonitor\\PowerStateChanged`,
+        payload: {
+            state: 'on-battery'
+        }
+    });
+})
+
+powerMonitor.addListener('thermal-state-change', (state: string) => {
+    notifyLaravel("events", {
+        event: `\\Native\\Laravel\\Events\\PowerMonitor\\ThermalStateChanged`,
+        payload: {
+            state
+        }
+    });
+})
+
+powerMonitor.addListener('speed-limit-change', (limit: number) => {
+    notifyLaravel("events", {
+        event: `\\Native\\Laravel\\Events\\PowerMonitor\\SpeedLimitChanged`,
+        payload: {
+            limit
+        }
+    });
+})
 
 export default router;
