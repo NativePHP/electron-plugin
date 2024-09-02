173448ab38bad737cfa9c40f545a8402dfb76488
Simon Hamp <simon.hamp@me.com>
2024-09-02 13:20:13 +0100

    Cleanup

--- src/server/api/system.ts
+++ src/server/api/system.ts
@@ -1,11 +1,12 @@
-import express from 'express'
+import express from 'express';
 import {BrowserWindow, systemPreferences, safeStorage} from 'electron';
+
 const router = express.Router();
 
 router.get('/can-prompt-touch-id', (req, res) => {
     res.json({
         result: systemPreferences.canPromptTouchID(),
-    })
+    });
 });
 
 router.post('/prompt-touch-id', async (req, res) => {
@@ -16,7 +17,7 @@ router.post('/prompt-touch-id', async (req, res) => {
     } catch (e) {
         res.status(400).json({
             error: e.message,
-        })
+        });
     }
 });
 
@@ -51,56 +52,56 @@ router.post('/decrypt', async (req, res) => {
 });
 
 router.get('/printers', async (req, res) => {
-  const printers = await BrowserWindow.getAllWindows()[0].webContents.getPrintersAsync();
+    const printers = await BrowserWindow.getAllWindows()[0].webContents.getPrintersAsync();
 
-  res.json({
-    printers,
-  });
+    res.json({
+        printers,
+    });
 });
 
 router.post('/print', async (req, res) => {
-  const {printer, html} = req.body;
+    const {printer, html} = req.body;
 
-  let printWindow = new BrowserWindow({
-    show: false,
-  });
-
-  printWindow.webContents.on('did-finish-load', () => {
-    printWindow.webContents.print({
-      silent: true,
-      deviceName: printer,
-    }, (success, errorType) => {
-      res.sendStatus(200);
+    let printWindow = new BrowserWindow({
+        show: false,
     });
-    printWindow = null;
-  });
 
-  await printWindow.loadURL(`data:text/html;charset=UTF-8,${html}`);
+    printWindow.webContents.on('did-finish-load', () => {
+        printWindow.webContents.print({
+            silent: true,
+            deviceName: printer,
+        }, (success, errorType) => {
+            res.sendStatus(200);
+        });
+        printWindow = null;
+    });
+
+    await printWindow.loadURL(`data:text/html;charset=UTF-8,${html}`);
 });
 
 router.post('/print-to-pdf', async (req, res) => {
-  const {html} = req.body;
+    const {html} = req.body;
 
-  let printWindow = new BrowserWindow({
-    show: false,
-  });
+    let printWindow = new BrowserWindow({
+        show: false,
+    });
 
-  printWindow.webContents.on('did-finish-load', () => {
-    printWindow.webContents.printToPDF({}).then(data => {
-        printWindow.close();
-            res.json({
-              result: data.toString('base64'),
+    printWindow.webContents.on('did-finish-load', () => {
+        printWindow.webContents.printToPDF({}).then(data => {
+            printWindow.close();
+                res.json({
+                    result: data.toString('base64'),
+                });
+        }).catch(e => {
+            printWindow.close();
+
+            res.status(400).json({
+                error: e.message,
             });
-      }).catch(e => {
-        printWindow.close();
-
-        res.status(400).json({
-          error: e.message,
         });
     });
-  });
 
-  await printWindow.loadURL(`data:text/html;charset=UTF-8,${html}`);
+    await printWindow.loadURL(`data:text/html;charset=UTF-8,${html}`);
 });
 
 export default router;
