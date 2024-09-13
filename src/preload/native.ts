d97c5ee5382bcec506f8a8fcf7036b9827f24a28
gwleuverink <willem@leuver.ink>
2024-09-13 02:36:46 +0200

    Strip leading slashes before matching event name

--- src/preload/native.ts
+++ src/preload/native.ts
@@ -3,11 +3,14 @@ import { ipcRenderer } from 'electron'
 export default {
     on: (event, callback) => {
         ipcRenderer.on('native-event', (_, data) => {
-            if(event !== data.event) {
-                return;
-            }
 
-            callback(data.payload);
+            // Strip leading slashes
+            event = event.replace(/^(\\)+/, '');
+            data.event = data.event.replace(/^(\\)+/, '');
+
+            if(event === data.event) {
+                return callback(data.payload);
+            }
         })
     }
 }
